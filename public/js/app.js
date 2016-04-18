1c20a8c4807e94a50751cdc4db0ccdd3a20e0dbe
Ric Allinson <allinson@yahoo-inc.com>
2016-04-17 19:54:42 -0700

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,75 @@
+(function() {
+
+  function getNodes() {
+    var nodes = [];
+    var id;
+    var space;
+    var item;
+    for (id in story.spaces) {
+      space = story.spaces[id];
+      nodes.push({
+        id: id,
+        label: space.description,
+        shape: 'box'
+      });
+    }
+    for (id in story.items) {
+      item = story.items[id];
+      nodes.push({
+        id: id,
+        label: item.description,
+        shape: 'eclipse',
+        color: '#7BE141'
+      });
+    }
+    return nodes;
+  }
+
+  function getEdges() {
+    var edges = [];
+    var connection;
+    for (id in story.spaces) {
+      for (i in story.spaces[id].connections) {
+        connection = story.spaces[id].connections[i];
+        edges.push({
+          from: id,
+          to: connection.id,
+          label: connection.description
+        });
+      }
+    }
+    return edges;
+  }
+
+  var container = document.getElementById('story-graph');
+  var data = {
+    nodes: getNodes(),
+    edges: getEdges()
+  };
+  var options = {
+    layout: {
+      randomSeed: 1000
+    },
+    edges: {
+      arrows: {
+        to: true
+      },
+      font: {
+        strokeWidth: 5,
+        strokeColor: 'white'
+      }
+    },
+    "physics": {
+      "hierarchicalRepulsion": {
+        "centralGravity": 0,
+        "springLength": 0,
+        "springConstant": 0,
+        "nodeDistance": 200,
+        "damping": 1
+      },
+      "minVelocity": 0.75,
+      "solver": "hierarchicalRepulsion"
+    }
+  };
+  var network = new vis.Network(container, data, options);
+}());
---
+++ public/js/story.js
@@ -0,0 +1,89 @@
+var story = {
+	spaces: {
+		'bobs house only room': {
+			description: 'As expected for such a small house,\nit only has one room.',
+			connections: [
+				{
+					id: 'bobs house front garden',
+					description: 'For such a small house the door seems out of proportion.'
+				},
+				{
+					id: 'bobs house back garden',
+					description: 'The garden looks abnormally big for a small house.'
+				}
+			]
+		},
+		'bobs house front garden': {
+			description: 'Bob front garden is a small affair.\nLets just say his green thumb fell off.',
+			connections: [
+				{
+					id: 'bobs house only room',
+					description: 'Foo.'
+				},
+				{
+					id: 'bobs house driveway',
+					description: 'Bar.'
+				},
+				{
+					id: 'post office',
+					description: 'Baz.'
+				}
+			]
+		},
+		'bobs house back garden': {
+			description: 'There seems to be no end to jungle Bob calls a garden.',
+			connections: [
+				{
+					id: 'bobs house driveway',
+					description: 'Moo.'
+				},
+				{
+					id: 'bobs house only room',
+					description: 'Coo.',
+					if: {
+						id: 'bobs house back door key',
+						description: 'Cew.'
+					}
+				}
+			]
+		},
+		'bobs house driveway': {
+			description: 'Single car driveway.',
+			connections: [
+				{
+					id: 'bobs house back garden',
+					description: 'Yew.'
+				},
+				{
+					id: 'bobs house front garden',
+					description: 'Few.'
+				},
+				{
+					id: 'post office',
+					description: 'New.'
+				}
+			]
+		},
+		'post office': {
+			description: 'Waking into the Post Office there is a single old wooden counter.',
+			connections: [
+				{
+					id: 'bobs house front garden',
+					description: 'Pew.'
+				},
+				{
+					id: 'bobs house driveway',
+					description: 'Pew.'
+				}
+			]
+		}
+	},
+	items: {
+		'bobs house back door key': {
+			description: 'A key to a door.',
+		}
+	},
+	'placements': {
+		'bobs house back door key': 'bobs house only room'
+	}
+};
