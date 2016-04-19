a94c9c0b85219ec977ea5d677455de04582f2a5a
Ric Allinson <allinson@yahoo-inc.com>
2016-04-19 00:25:31 -0700

    More items

--- public/js/reader.js
+++ public/js/reader.js
@@ -1,28 +1,112 @@
 (function () {
 
-	// Render a space and it's connections.
-	function renderSpace(id) {
-		var space = story.spaces[id];
-		if (!space) {
-			return;
-		}
-		$('#reader').append('<p>' + space.description + '</p>');
-		for (id in space.connections) {
-			connection = space.connections[id];
-			$('#reader').append('<p class="follow" data-connection-id="' + id + '">' + connection.description + '</p>');
-		}
-	}
+    var readerHistory = [];
 
-	// Start the story.
-	renderSpace($('#reader').data('space-id'));
+    function addToArray(array, item) {
+        var i;
+        for (i in array) {
+            if (array[i] == item) {
+                return array;
+            }
+        }
+        array.push(item);
+        return array;
+    }
 
-	// Follow the story.
-	$('#reader').click(function (e) {
-		var id = $(e.target).data('connection-id');
-		if (id) {
-			$('#reader').find('.follow').removeClass('follow');
-			renderSpace(id);
-		}
-	});
+    function removeFromArray(array, item) {
+        var i = array.indexOf(item);
+        if (i > -1) {
+            array.splice(i, 1);
+            return array;
+        }
+        return array;
+    }
+
+    function updateReader(id) {
+        readerHistory.push(id);
+        console.log(readerHistory);
+    }
+
+    // Take an item and keep it.
+    function pickUpItem(spaceId, itemId) {
+        // Remove item from space.
+        story.placements[spaceId] = removeFromArray(story.placements[spaceId], itemId);
+        // Add item to readers list.
+        story.placements._reader = addToArray(story.placements._reader, itemId);
+        console.log(story.placements);
+    }
+
+    // Descriptions can be a string or array. In the case of an array one item is randomly selected.
+    function getDescription(description) {
+        if (!Array.isArray(description)) {
+            return description;
+        }
+        var index = Math.round(Math.random() * (description.length - 1));
+        return description[index];
+    }
+
+    // Render a space and it's connections.
+    function renderSpace(id) {
+        var space = story.spaces[id];
+        var items = story.placements[id];
+        var i;
+        var p;
+        var connection;
+        var count = 0;
+        if (!space) {
+            return;
+        }
+        p = $('<p/>');
+        p.append('<span>' + getDescription(space.description) + '</span> ')
+        for (spaceId in space.connections) {
+            connection = space.connections[spaceId];
+            if (connection.if) {
+                // Deal with `if` statments here.
+                p.append('<span>' + getDescription(connection.description) + '</span> ');
+            } else {
+                p.append('<span class="follow" data-connection-id="' + spaceId + '">' + getDescription(connection.description) + '</span> ');
+                count++;
+            }
+        }
+
+        if (items && items.length > 0) {
+            for (i in items) {
+                itemId = items[i];
+                p.append('<span class="follow item" data-space-id="' + id + '" data-item-id="' + itemId + '">' + getDescription(story.items[itemId].description) + '</span> ');
+            }
+        }
+        updateReader(id);
+        $('#reader').append(p);
+        if (count == 1 ) {
+            // Render the last connection-id.
+            renderSpace(id);
+        }
+    }
+
+    // Start the story.
+    $('h1.title').text(story.title);
+    renderSpace($('#reader').data('space-id'));
+
+    // Follow the story.
+    $('#reader').click(function (e) {
+        var id = $(e.target).data('connection-id');
+        if (id) {
+            $('#reader').find('.follow').data('connection-id', null);
+            $('#reader').find('.follow').removeClass('follow');
+            renderSpace(id);
+        }
+    });
+
+    // pick up an item.
+    $('#reader').click(function (e) {
+        var itemId = $(e.target).data('item-id');
+        var spaceId = $(e.target).data('space-id');
+        if (itemId && spaceId) {
+            $(e.target).data('item-id', null);
+            $(e.target).data('space-id', null);
+            $(e.target).removeClass('follow');
+            pickUpItem(spaceId, itemId);
+        }
+    });
 
 }());
--- public/js/story.js
+++ public/js/story.js
@@ -1,77 +1,94 @@
 var story = {
+	title: "Bobs Lonely Life",
 	spaces: {
 		'bobs house only room': {
-			description: 'As expected for such a small house,\nit only has one room.',
+			description: 'As expected for such a small house, Bobs place consists of one solitary room.',
 			connections: {
 				'bobs house front garden': {
-					description: 'For such a small house the door seems out of proportion.'
+					description: [
+						'There is a window looking out to the front garden with small door to the right of it.',
+						'The door, as new as it appears, is missing the most fundamental of things a front door to home should own. A lock.'
+					]
 				},
 				'bobs house back garden': {
-					description: 'The garden looks abnormally big for a small house.'
+					description: 'The back wall has nothing but shelves full of paint. Perfectly positioned in the middle is a large cat flap.'
 				}
 			}
 		},
 		'bobs house front garden': {
-			description: 'Bob front garden is a small affair.\nLets just say his green thumb fell off.',
+			description: 'Bobs front garden is a small affair and not a pretty one. Lets just say his green thumb fell off many years earlier.',
 			connections: {
 				'bobs house only room': {
-					description: 'Foo.'
+					description: 'The door closes as slowly as a snail rushing home for creamy Escargot. The long missing lock takes a sigh of want.'
 				},
 				'bobs house driveway': {
-					description: 'Bar.'
+					description: 'Not six feet away is the driveway that once held a magnificent automotive specimen.'
 				},
 				'post office': {
-					description: 'Baz.'
+					description: 'But without question is the best view in the village is directly opposite Bobs front door. The Post Office window.'
 				}
 			}
 		},
 		'bobs house back garden': {
-			description: 'There seems to be no end to jungle Bob calls a garden.',
+			description: 'It takes some effort to squeeze through the deceptively small cat flap. Looking about there seems to be no end to the jungle Bob calls a his back garden.',
 			connections: {
-				'bobs house driveway': {
-					description: 'Moo.'
-				},
 				'bobs house only room': {
-					description: 'Coo.',
+					description: 'The cat flap slams shut and regardless of how much mud is thrown, it will not budge one bit.',
 					if: {
 						id: 'bobs house back door key',
-						description: 'Cew.'
+						description: 'The rusty old cat flap has a worn area that looks remarkably like the shape of a bottle opener.'
 					}
+				},
+				'bobs house driveway': {
+					description: 'Off to the right, hidden between the back of Bobs house and a blackberry bush is a well worn path.'
 				}
 			}
 		},
 		'bobs house driveway': {
-			description: 'Single car driveway.',
+			description: 'More blackberry bush than driveway this part of Bobs estate had most defiantly seen better days.',
 			connections: {
 				'bobs house back garden': {
-					description: 'Yew.'
+					description: 'Not the most inviting of views, being mostly blackberry bush, but there is evidence of a pathway heading down the side of Bobs house.'
 				},
 				'bobs house front garden': {
-					description: 'Few.'
+					description: 'One could ponder for hours about how such a small front garden could be so well neglected.'
 				},
 				'post office': {
-					description: 'New.'
+					description: 'From the driveway there is a clear view of the Post Office counter. Only obscured if one of the punters leaves the door open.'
 				}
 			}
 		},
 		'post office': {
-			description: 'Waking into the Post Office there is a single old wooden counter.',
+			description: 'Waking into the Post Office there is an old wooden counter with the standard issue glass partition keeping away any unwanted advances. Behind said glass is the view many have died for, Mrs Molly Malone.',
 			connections: {
 				'bobs house front garden': {
-					description: 'Pew.'
+					description: 'Opposite from Mrs Molly Malone\'s Post Office is Bobs house. If one can claim as such in todays world of health and safety.'
 				},
 				'bobs house driveway': {
-					description: 'Mew.'
+					description: 'Mrs Molly Malone longs for customers to leave the Post Office door open. For the single reason that it blocks her view of Bobs bush.'
 				}
 			}
 		}
 	},
 	items: {
 		'bobs house back door key': {
-			description: 'A key to a door.',
+			description: 'An old bottle opener with scratches on one side lays there, longing for a beer to open.',
+			'owned': {
+				description: 'A rusty bottle opener.'
+			},
+			'take': {
+				description: 'Like an old school magicians trick the bottle opener vanishes from sight.'
+			},
+			'leave': {
+				description: 'To the sound of a duck passing wind a small bottle opener appears on the ground.'
+			},
+			'use': {
+				description: 'As if designed by the greatest minds of Silicon Valley the bottle opener perfectly positioned gains entry to Bobs back door.'
+			}
 		}
 	},
 	'placements': {
-		'bobs house back door key': 'bobs house only room'
+		'bobs house only room': ['bobs house back door key'],
+		'_reader': []
 	}
 };
